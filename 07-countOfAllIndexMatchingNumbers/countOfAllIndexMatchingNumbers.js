595a9d4eade3f594f213ef96d2633213f47a2845
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 19:13:55 -0400

    Solve countOfAllIndexMatchingNumbers.js Function

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,12 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  let count = 0;
+  nums.forEach((nums, index) => {
+    if (nums === index) {
+      count++;
+    }
+  })
+  return count;
 }
 
 // Do not edit this line;
