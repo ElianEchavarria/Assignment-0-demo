567de964d8d7bd755131479e1287401755bb8636
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 18:48:30 -0400

    Solve countOfAllNumbersSmallerThanTarget.js Function

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  let count = 0;
+  nums.forEach((values) => {
+    if (values < target) {
+      count++;
+    }
+  })
+  return count
 }
 
 // Do not edit this line;
