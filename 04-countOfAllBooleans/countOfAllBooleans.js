2a4493da16a9697e530fbee596afcaa2d642d255
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 18:17:38 -0400

    Solve countOfAllBooleans.js Function

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,14 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let count = 0;
+  arr.map((values) => {
+    if (typeof values === "boolean") {
+      count++
+    }
+    return
+  })
+
+  return count
 }
 
 // Do not edit this line;
