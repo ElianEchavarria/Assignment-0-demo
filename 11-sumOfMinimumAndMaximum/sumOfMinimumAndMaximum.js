d29811881213d7a384c739afceab8b8190e11d05
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 21:00:08 -0400

    Solve sumOfMinimumAndMaximum.js Function

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let min = nums[0];
+  let max = nums[0];
+  nums.forEach((values) => {
+    if (value < min) {
+      min = value;
+    }
+    if (value > max) {
+      max = value;
+    }
+  })
+
+  return min + max;
 }
 
 // Do not edit this line;
