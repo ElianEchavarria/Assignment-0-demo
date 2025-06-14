96b8986f99690dd553dda8785b67dc79ceba2774
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 20:41:14 -0400

    Solve sumOfAllOddNumbers.js Function

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  let sum = 0;
+  nums.forEach((values) => {
+    if (values % 2 !== 0) {
+      sum += values
+    }
+  })
+  return sum;
 }
 
 // Do not edit this line;
