038728d1c59973d497f7c653a9f27ec1ff43d2b6
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 22:38:29 -0400

    Solve pairSum Function

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,13 @@
 function pairSum(nums, target) {
   // Insert code here;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
