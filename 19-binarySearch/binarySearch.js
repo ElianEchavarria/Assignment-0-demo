2b4e14658da97643b837a33705cc09ef194b6e8e
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 22:47:26 -0400

    Solve binarySearch.js Function

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,8 +3,18 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
-    // Insert code here;
+  binarySearch(nums, target, left = 0, right = nums.length - 1) {
+    if (left > right) return false;
+
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] === target) {
+      return true;
+    } else if (target < nums[middle]) {
+      return this.binarySearch(nums, target, left, middle - 1);
+    } else {
+      return this.binarySearch(nums, target, middle + 1, right);
+    }
   }
 }
 
