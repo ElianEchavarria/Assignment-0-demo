fb086fbd30ef883384bb0674d69e5fb479f50b48
Elian Echavarria <elianstanly431@gmail.com>
2025-06-13 20:50:53 -0400

    Solve sumOfNumsWithinARange.js Function

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let count = 0;
+  nums.forEach((values) => {
+    if (values >= start && values <= end) {
+      count++;
+    }
+  })
+  return count;
 }
 
 // Do not edit this line;
