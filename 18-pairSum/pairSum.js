ca0bad4f7baf52b0c54518aa35bbc5aae70b7b4d
Elian Echavarria <elianstanly431@gmail.com>
2025-06-15 15:59:03 -0400

    Fix pairSum function

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,9 @@
 function pairSum(nums, target) {
   // Insert code here;
+  if (nums.length <= 1) {
+    throw new Error("Array must have at least two elements");
+  }
+
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (i !== j && nums[i] + nums[j] === target) {
